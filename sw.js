602df89e893ca7648d2f65ab4ff2828737ea2e91
​ <>
2026-06-03 20:51:14 -0700

    pwa

---
+++ sw.js
@@ -0,0 +1,36 @@
+
+// Establish a cache name
+const cacheName = 'MyFancyCacheName_v1';
+
+// Assets to precache
+const precachedAssets = [
+  './',
+];
+
+self.addEventListener('install', (event) => {
+  // Precache assets on install
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+    return cache.addAll(precachedAssets);
+  }));
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      // Go to the cache first
+      return cache.match(event.request.url).then((cachedResponse) => {
+        // Return a cached response if we have one
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        // Otherwise, hit the network
+        return fetch(event.request).then((fetchedResponse) => {
+          // Add the network response to the cache for later visits
+          cache.put(event.request, fetchedResponse.clone());
+
+          // Return the network response
+          return fetchedResponse;
+        });
+      });
+    }));
+});
